f76061d1606b7adc8eb1d77de5078ebbb9ba1311
AIE\s171372 <sonicxeps@gmail.com>
2017-02-15 16:36:13 +1100

    Added 3D Camera & Sun

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+#include "gl_core_4_4.h"
+#include "GLFW\glfw3.h"
+#include "AIE\Gizmos.h"
+#include "GLM\glm.hpp"
+#include "GLM\ext.hpp"
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
 
 int main() {
+
+
+	if (glfwInit() == false)
+		return -1;
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "OpenGL", nullptr, nullptr);
+
+	if (window == nullptr) {
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+
+	Gizmos::create();
+
+	// Camera
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0,1,0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.0f);
+
+	auto major = ogl_GetMajorVersion();
+	auto minor = ogl_GetMinorVersion();
+	printf("GL: %i.%i\n", major, minor);
+
+	glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		Gizmos::clear();
+		Gizmos::addTransform(glm::mat4(1));
+
+		mat4 sunRotation = mat4(5);
+
+		// colors
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+
+		Gizmos::addSphere(vec3(0, 0, 0), 1.f, 20, 20, vec4(1, 0, 0, 1), &sunRotation);
+
+		// Grid
+		for (int i = 0; i < 21; ++i) {
+			Gizmos::addLine(vec3(-10 + i, 0, 10), vec3(-10 + i, 0, -10), i == 10 ? white : black);
+			Gizmos::addLine(vec3(10, 0, -10 + i), vec3(-10, 0, -10 + i), i == 10 ? white : black);
+		}
+
+		Gizmos::draw(projection * view);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	Gizmos::destroy();
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
